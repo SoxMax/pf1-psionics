46f593c735c694551780c8c11b733f9ed7754a46
SoxMax <irishsoxmax@gmail.com>
2025-11-13 20:43:02 -0500

    add null catch

--- scripts/documents/action/action-use.mjs
+++ scripts/documents/action/action-use.mjs
@@ -28,7 +28,7 @@ export function pf1PreActionUseHook(actionUse) {
       return false;
     }
     const currentFocus = actionUse.actor.flags?.[MODULE_ID]?.focus?.current || 0;
-    const focusCost =  actionUse.shared.rollData.focusCost || 0;
+    const focusCost =  actionUse.shared.rollData?.focusCost || 0;
     if (focusCost > currentFocus) {
       ui.notifications.error(game.i18n.localize("PF1-Psionics.Error.NotEnoughFocus"));
       return false;
