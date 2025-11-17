cf2c60e75e064fdaf4f37b38246be5db5e83fc65
SoxMax <irishsoxmax@gmail.com>
2025-11-17 13:16:22 -0500

    use manifestor

--- scripts/documents/action/action.mjs
+++ scripts/documents/action/action.mjs
@@ -1,18 +1,19 @@
-import { MODULE_ID } from "../../_module.mjs";
+import {MODULE_ID} from "../../_module.mjs";
 
 export function injectItemAction() {
-  libWrapper.register(MODULE_ID, "pf1.components.ItemAction.prototype.getDC", function (wrapped, rollData) {
+  libWrapper.register(MODULE_ID, "pf1.components.ItemAction.prototype.getDC", function(wrapped, rollData) {
     if (this.item.type === `${MODULE_ID}.power`) {
       rollData ??= this.getRollData();
 
       // Get conditional save DC bonus
       /** @type {number} */
       const dcBonus = rollData.dcBonus ?? 0;
-
-      const psibook = this.item.psibook;
-      if (psibook) {
+      debugger;
+      /** @type {object} */
+      const manifestor = this.item.manifestor;
+      if ( manifestor) {
         /** @type {string} */
-        let formula = psibook.baseDCFormula;
+        let formula =  manifestor.baseDCFormula;
 
         /** @type {object} - Item action "data" */
         const data = rollData.action;
