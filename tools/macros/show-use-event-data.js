c4b7b7c4ceca21a16d8016c251cf05ba552b80ec
SoxMax <irishsoxmax@gmail.com>
2025-12-19 21:27:15 -0500

    add test macro

---
+++ tools/macros/show-use-event-data.js
@@ -0,0 +1,9 @@
+console.group("[PF1 Psionics] Use Event Data");
+console.log({
+  "item": item,
+  "actor": actor,
+  "action": action,
+  "token": token,
+  "shared": shared,
+})
+console.groupEnd();
